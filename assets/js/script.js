3bd68aa43611d9b0681fd09e35c5df02b6f38c53
Christian Anderson <40026509+ChristianFlows@users.noreply.github.com>
2021-03-10 12:07:02 -0500

    minor style changes and added initial number-letter-special char generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,44 @@
 // Assignment code here
 
-// Random Character Generation
+// Obj to store selected password arguments
+var randomDataObj = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    special: getRandomSpecial
+};
+
+// Generate password function
+function generatePassword(lower, upper, number, special) {
+
+    // Accept PW Variables
+    var pwVars = upper + lower + number + special;
+    // Filter PW Variables
+    //Array to sort here
+    // Loop generator function to input each type
+
+    // Password generated
+
+}
+
+// Random lowercase, uppercase, numeric, &&|| special character Generation
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSpecial() {
+    var special = "\\!#$%&'()*+,-./:;<=>?@[]^_`{|}~\"";
+    return special[Math.floor(Math.random() * special.length)];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
